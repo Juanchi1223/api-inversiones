ea8831751ac5c0a159779d3414cab5e3acf53e19
Juan Jose Canestrari <juanchicanestrari@gmail.com>
2026-06-24 17:56:00 -0300

    feat: initial configuration commit

---
+++ src/main/java/com/juanchi1223/apiinversiones/ApiInversionesApplication.java
@@ -0,0 +1,13 @@
+package com.juanchi1223.apiinversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInversionesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiInversionesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/juanchi1223/apiinversiones/ApiInversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchi1223.apiinversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInversionesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
